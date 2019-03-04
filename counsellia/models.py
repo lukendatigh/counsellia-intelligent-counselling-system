41873434a644dc88746ca70a839fbd7a0232a7d0
lukendatigh <lukendatigh@gmail.com>
2019-03-04 12:05:10 +0100

    refactor and minor fixes

--- counsellia/models.py
+++ counsellia/models.py
@@ -5,15 +5,15 @@ from users.models import Counsellor, Counsellee
 
 class Appointment(models.Model):
 	TYPE = (
-		('first', 'first'),
-		('follow-up', 'follow-up'),
-		('final', 'final'),
+		('first', 'First appointment'),
+		('follow-up', 'Follow-up appointment'),
+		('final', 'Final appointment'),
 	)
-	description = models.CharField('short description', max_length=128, null=True, blank=True)
+	description = models.CharField('Short Description', max_length=128, null=True, blank=True)
 	counsellee = models.ForeignKey(Counsellee, on_delete=models.CASCADE, null=True)
 	counsellor = models.ForeignKey(Counsellor, on_delete=models.CASCADE, null=True)
 	time = models.DateTimeField()
-	appointment_type = models.CharField(max_length=20, choices=TYPE, null=True)
+	appointment_type = models.CharField('Appointment Type', max_length=20, choices=TYPE, null=True)
 	requested = models.BooleanField(default=True, null=True)
 	fixed = models.BooleanField(default=False, null=True)
 	held = models.BooleanField(default=False, null=True)
@@ -34,15 +34,15 @@ class Report(models.Model):
 	counsellor = models.ForeignKey(Counsellor, on_delete=models.CASCADE, null=True)
 	description = models.CharField('short description', max_length=256, null=True)
 	time = models.DateTimeField(default=timezone.now, blank=True, null=True)
-	text = models.TextField(null=True)
-	topic = models.TextField(null=True)
-	word_count = models.TextField(null=True)
-	sentiment = models.TextField(null=True)
-	remarks = models.TextField(null=True)
+	text = models.TextField(blank=True, null=True)
+	topic = models.TextField(blank=True, null=True)
+	word_count = models.TextField(blank=True, null=True)
+	sentiment = models.TextField('sentiment analysis', null=True)
+	remarks = models.TextField(blank=True, null=True)
 	archived = models.BooleanField(default=False, null=True)
 
 	def __str__(self):
 		return self.description
 
 	class Meta:
-		ordering = ['-time',]
+		ordering = ['counsellee', '-time',]
