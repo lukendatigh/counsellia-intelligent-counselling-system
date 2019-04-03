0eea7fe7bfe10c79f8f866006b04e9474ca6150e
lukendatigh <lukendatigh@gmail.com>
2019-04-03 12:57:59 +0100

    refactor counsellor form layouts

--- counsellors/forms.py
+++ counsellors/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from users.models import ( User, Counsellor )
+from users.models import User, Counsellor
+from counsellia.models import Appointment
+
 
 class UserUpdateForm(forms.ModelForm):
 	class Meta:
 		model = User
-		fields = ['username', 'first_name', 'last_name']
+		fields = ['username', 'first_name', 'last_name', 'email']
 		help_texts = {
 			'username': None,
 		}
@@ -14,8 +16,29 @@ class ProfileUpdateForm(forms.ModelForm):
 		model = Counsellor
 		fields = ['specialties', 'quote', 'image', 'dob', 'gender', 'website', 'address', 'phone_number', 'bio', 'qualification', 'available']
 		widgets = {
-			'specialties': forms.SelectMultiple(attrs={'size':9}),
+			'specialties': forms.SelectMultiple(attrs={'size':12}),
 			'dob': forms.TextInput(attrs={'type': 'date'}),
 			'bio': forms.Textarea(attrs={'rows':3}),
 			'qualification': forms.Textarea(attrs={'rows':3}),
+		}
+
+
+class AppointmentCreateForm(forms.ModelForm):
+	class Meta:
+		model = Appointment
+		fields = ['description', 'remarks', 'time', 'appointment_type']
+		widgets = {
+			'time': forms.DateInput(attrs={'type': 'date'}),
+			'remarks': forms.Textarea(attrs={'rows':4}),
+		}
+
+
+class AppointmentEditForm(forms.ModelForm):
+	class Meta:
+		model = Appointment
+		fields = ['description', 'remarks', 'recommendations', 'fixed', 'held', 'time', 'counsellor_archived']
+		widgets = {
+			'time': forms.DateInput(attrs={'type': 'date'}),
+			'recommendations': forms.Textarea(attrs={'rows':3}),
+			'remarks': forms.Textarea(attrs={'rows':4}),
 		}
