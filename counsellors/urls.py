d0b680b7fac1cab2ca98f45cbc895fff344d1f8c
lukendatigh <lukendatigh@gmail.com>
2019-04-03 13:06:12 +0100

    update counsellors urls

--- counsellors/urls.py
+++ counsellors/urls.py
@@ -1,23 +1,66 @@
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 from . import views as counsellor_views
-
 from . views import (
-		AppointmentListView,
 		AppointmentDetailView,
-		AppointmentCreateView,
-		AppointmentUpdateView,
+		AppointmentEditView,
 		AppointmentDeleteView,
 		CounselleeListView,
-		CounselleeProfileView,
+		AppointmentsUpcomingView,
+		AppointmentsRequestsView,
+		AppointmentsHeldView,
+		AppointmentsArchivedView,
 	)
 
+
+
 urlpatterns = [
+
+	# access analysis urls
+	path('report/', include('analysis.urls')),
+
+	# user's profile
 	path('profile/', counsellor_views.profile_update, name = 'counsellor-profile'),
 	
-	path('counsellee/<int:pk>', CounselleeProfileView.as_view(), name = 'selected-counsellee-profile'),
-	path('', AppointmentListView.as_view(), name = 'counsellor-schedule'),	
-	path('counsellees', CounselleeListView.as_view(), name = 'counsellor-counsellee-list'),
+
+	# appointment create, update, delete and view details
+	path('counsellor/<int:pk>/appointment/', 
+		counsellor_views.appointment_create, 
+		name = 'counsellor-appointment-create'),
+	path('appointment/<int:pk>/', 
+		AppointmentDetailView.as_view(),
+		name = 'counsellor-appointment-detail'),
+	path('appointment/<int:pk>/edit/',
+		AppointmentEditView.as_view(success_url = "/counsellor/appointments/requested/"),
+		name = 'counsellor-appointment-edit'),
+	path('appointment/<int:pk>/delete/',
+		AppointmentDeleteView.as_view(),
+		name = 'counsellor-appointment-delete'),
+
+
+
+	# apoointment lists (pending, upcoming, held, archived)
+	path('appointments/upcoming/',
+		AppointmentsUpcomingView.as_view(),
+		name = 'counsellor-appointments-upcoming'),
+	path('appointments/requested/',
+		AppointmentsRequestsView.as_view(),
+		name = 'counsellor-appointments-requests'),
+	path('appointments/held/',
+		AppointmentsHeldView.as_view(),
+		name = 'counsellor-appointments-held'),
+	path('appointments/archived/',
+		AppointmentsArchivedView.as_view(),
+		name = 'counsellor-appointments-archived'),
+
+
+
+	# counsellee list and profile
+	path('counsellee/<int:pk>', 
+		counsellor_views.counsellee_profile, 
+		name = 'selected-counsellee-profile'),
+	path('counsellees/', 
+		CounselleeListView.as_view(), 
+		name = 'counsellor-counsellee-list'),
 
 ]
